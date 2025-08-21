9f6c4dd21fcbb67c4c2f49904bb65e45ecdf2a1e
Dhruv <dhruvrohilla9350@gmail.com>
2025-08-21 16:23:09 +0530

    provider add

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.40.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "4a8937a7-b294-4a1f-8d31-79bbb1a4c17f"
+}
